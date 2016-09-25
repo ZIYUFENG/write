730e9bb4ae6e60c2c960ba2bcf18c0d682514fec
帝鲮 <jlan940482458@qq.com>
2016-09-25 09:37:00 +0800

    数据存取更新

--- app/src/main/java/com/dealin/writer/DataManager.java
+++ app/src/main/java/com/dealin/writer/DataManager.java
@@ -7,8 +7,38 @@ package com.dealin.writer;
  */
 
 public class DataManager {
-    //保存版本名称到本地
+    /**保存版本名称到本地
+     * 将参数中的versionName保存在软件设置目录下的veirsionName文件中
+     * PS.软件设置目录在类Config中能找到，只需要直接使用
+     * @param versionName 新的版本名称
+     */
     public static void saveVersionName(String versionName) {
 
+    }
+
+    /**
+     * 从本地将versionName读取出来
+     * @return
+     */
+    public static String getVersionName() {
+        return null;
+    }
+
+    /**
+     * 实现saveFile和readFile方法，方便数据存取
+     * @param path 文件路径;
+     * @param content 文件内容;
+     */
+    public static void saveFile(String path,String content) {
+
+    }
+
+    /**
+     * 读取文件
+     * @param path 文件路径
+     * @return
+     */
+    public static String readFile(String path) {
+        return null;
     }
 }
