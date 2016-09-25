0736c36806d7aab90bc8c7adce0771d57de5e7b9
帝鲮 <jlan940482458@qq.com>
2016-09-25 09:23:19 +0800

    新增数据管理工具类

---
+++ app/src/main/java/com/dealin/writer/DataManager.java
@@ -0,0 +1,14 @@
+package com.dealin.writer;
+
+/**
+ * Created by 兰件 on 2016/9/25.
+ * 这是一个读取数据的工具类
+ * 所有数据存取工作都在这里进行
+ */
+
+public class DataManager {
+    //保存版本名称到本地
+    public static void saveVersionName(String versionName) {
+
+    }
+}
